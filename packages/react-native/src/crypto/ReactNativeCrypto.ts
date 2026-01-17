2ca06f6b9b24ebef2ef976cd33c29a2b8520b1f2
luxni <307993459@qq.com>
2026-01-17 16:43:11 +0800

    fix sign error on new react-native-quick-crypto
    sign function only support der format at react-native-quick-crypto@1.0.6

--- packages/react-native/src/crypto/ReactNativeCrypto.ts
+++ packages/react-native/src/crypto/ReactNativeCrypto.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Crypto, Entropy, Environment, StandardCrypto, WebCrypto } from "#general";
+import { Bytes, Crypto, CryptoError, EcdsaSignature, Entropy, Environment, StandardCrypto, WebCrypto } from "#general";
 import { Buffer } from "@craftzdog/react-native-buffer";
 import QuickCrypto from "react-native-quick-crypto";
 
@@ -18,19 +18,78 @@ if (!("Buffer" in globalThis)) {
     (globalThis as unknown as { Buffer: typeof Buffer }).Buffer = Buffer;
 }
 
+const SIGNATURE_ALGORITHM = <EcdsaParams>{
+    name: "ECDSA",
+    namedCurve: "P-256",
+    hash: { name: "SHA-256" },
+};
+
 /**
  * Crypto implementation for React Native should work with a WebCrypto basis with 1.x
  */
 export class ReactNativeCrypto extends StandardCrypto {
     override implementationName = "ReactNativeCrypto";
+    #subtle: SubtleCrypto;
+
+    constructor() {
+        super(crypto as unknown as WebCrypto);
+        this.#subtle = (crypto as unknown as WebCrypto).subtle;
+    }
 
     static override provider() {
-        return new ReactNativeCrypto(crypto as unknown as WebCrypto);
+        return new ReactNativeCrypto();
+    }
+
+    override async signEcdsa(key: JsonWebKey, data: Bytes | Bytes[]) {
+        if (Array.isArray(data)) {
+            data = Bytes.concat(...data);
+        }
+
+        const { crv, kty, d, x, y } = key;
+
+        key = {
+            kty,
+            crv,
+            d,
+            x,
+            y,
+            ext: true, // Required by some subtle implementations to sign
+            key_ops: ["sign"],
+        };
+
+        const subtleKey = await this.importKey("jwk", key, SIGNATURE_ALGORITHM, false, ["sign"]);
+        try {
+            const derSignature = await this.#subtle.sign(SIGNATURE_ALGORITHM, subtleKey, Bytes.exclusive(data));
+            return new EcdsaSignature(Bytes.of(derSignature), "der");
+        } catch (e) {
+            throw new CryptoError(`Signature sign failed: ${e}`);
+        }
+    }
+
+    override async verifyEcdsa(key: JsonWebKey, data: Bytes, signature: EcdsaSignature) {
+        const { crv, kty, x, y } = key;
+        const publicKey = { crv, kty, x, y };
+        const subtleKey = await this.importKey("jwk", publicKey, SIGNATURE_ALGORITHM, false, ["verify"]);
+
+        try {
+            const verified = await this.#subtle.verify(
+                SIGNATURE_ALGORITHM,
+                subtleKey,
+                Bytes.exclusive(signature.der),
+                Bytes.exclusive(data),
+            );
+
+            if (!verified) {
+                throw new Error("Signature verification failed");
+            }
+        } catch (e) {
+            throw new CryptoError(`Signature verification failed: ${e}`);
+        }
     }
 }
 
 {
-    const rnCrypto = new ReactNativeCrypto(crypto as unknown as WebCrypto);
+    const rnCrypto = new ReactNativeCrypto();
     Environment.default.set(Entropy, rnCrypto);
     Environment.default.set(Crypto, rnCrypto);
 }
