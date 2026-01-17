9e06e5003f8b795a31e3a3e8265c9598fc959b55
luxni <307993459@qq.com>
2026-01-17 16:57:51 +0800

    remove ReactNativeEnvironment polyfill

--- packages/react-native/src/environment/register.ts
+++ packages/react-native/src/environment/register.ts
@@ -4,15 +4,15 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Boot, Environment } from "@matter/general";
-import { ReactNativeEnvironment } from "./ReactNativeEnvironment.js";
-
-// Mainly import TextEncoder and TextDecoder polyfill
-// @ts-expect-error No types but all fine
-import { polyfill } from "react-native-polyfill-globals/src/encoding";
-
-polyfill();
-
-Boot.init(() => {
-    Environment.default = ReactNativeEnvironment();
-});
+// import { Boot, Environment } from "@matter/general";
+// import { ReactNativeEnvironment } from "./ReactNativeEnvironment.js";
+//
+// // Mainly import TextEncoder and TextDecoder polyfill
+// // @ts-expect-error No types but all fine
+// import { polyfill } from "react-native-polyfill-globals/src/encoding";
+//
+// polyfill();
+//
+// Boot.init(() => {
+//     Environment.default = ReactNativeEnvironment();
+// });
